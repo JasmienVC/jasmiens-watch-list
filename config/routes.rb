32955b1cffbd465928f7f3e5248eabc685be7879
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-18 14:54:04 +0200

    lists controller functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists', to: 'lists#index'
-  get 'lists/:id', to: 'lists#show'
   get 'lists/new', to: 'lists#new'
+  get 'lists/:id', to: 'lists#show', as: 'list'
   post 'lists', to: 'lists#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
