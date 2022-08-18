9abc3f89fdf95ee51ff6d4374bfc2cb65650eb6f
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-18 12:57:09 +0200

    created list routes, controller, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists', to: 'lists#index'
+  get 'lists/:id', to: 'lists#show'
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
